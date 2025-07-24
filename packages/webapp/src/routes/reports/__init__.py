1f3f41497b78244d60833cc4ceda7e3576634a1c
Christopher Caston <chris@caston.id.au>
2025-07-24 17:12:25 +0800

    update reports.py

--- packages/webapp/src/routes/reports/__init__.py
+++ packages/webapp/src/routes/reports/__init__.py
@@ -2,12 +2,12 @@ from .tax import tax_bp
 from .sales import sales_bp
 from .financial import financial_bp
 from .expenses import expenses_bp
-from .utils import utils_bp
-from .utils import get_date_range
+#from .utils import utils_bp
+
 
 def register_reports_blueprints(app):
     app.register_blueprint(tax_bp)
     app.register_blueprint(sales_bp)
     app.register_blueprint(financial_bp)
     app.register_blueprint(expenses_bp)
-    app.register_blueprint(utils_bp)
+  #  app.register_blueprint(utils_bp)
