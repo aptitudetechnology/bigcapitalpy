b7494400fe079581a29dedd6b27f926f56f11b13
Christopher Caston <chris@caston.id.au>
2025-07-24 18:34:56 +0800

    added debug pycycle to requirements.txt

--- packages/webapp/src/routes/reports/__init__.py
+++ packages/webapp/src/routes/reports/__init__.py
@@ -1,9 +1,7 @@
-# Removed top-level blueprint imports to prevent circular dependencies.
-# from .tax import tax_bp
-# from .sales import sales_bp
-# from .financial import financial_bp
-# from .expenses import expenses_bp
-# from .utils import utils_bp
+from flask import Blueprint
+
+# Define the main reports blueprint
+reports_bp = Blueprint('reports', __name__, url_prefix='/reports')
 
 
 def register_reports_blueprints(app):
@@ -11,7 +9,7 @@ def register_reports_blueprints(app):
     Registers all report-related blueprints with the Flask application.
     Imports are placed inside this function to prevent circular import issues.
     """
-    # Import blueprints here to avoid circular dependencies at module load time
+    # Import sub-blueprints here to avoid circular dependencies at module load time
     from .tax import tax_bp
     from .sales import sales_bp
     from .financial import financial_bp
@@ -19,8 +17,12 @@ def register_reports_blueprints(app):
     # If utils_bp is ever used as a blueprint, uncomment and import it here
     # from .utils import utils_bp 
 
-    app.register_blueprint(tax_bp)
-    app.register_blueprint(sales_bp)
-    app.register_blueprint(financial_bp)
-    app.register_blueprint(expenses_bp)
-    # app.register_blueprint(utils_bp) # Uncomment if utils_bp is a blueprint to be registered
+    # Register sub-blueprints to the main reports_bp
+    reports_bp.register_blueprint(tax_bp)
+    reports_bp.register_blueprint(sales_bp)
+    reports_bp.register_blueprint(financial_bp)
+    reports_bp.register_blueprint(expenses_bp)
+    # reports_bp.register_blueprint(utils_bp) # Uncomment if utils_bp is a blueprint to be registered
+
+    # Register the main reports_bp with the app
+    app.register_blueprint(reports_bp)
