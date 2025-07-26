1d77c023910439bbf96805758a10c2fcd1ca3682
Christopher Caston <chris@caston.id.au>
2025-07-26 17:16:27 +0800

    updated backup index.html to include users e-mail

--- ssl/gen-cert.sh
+++ ssl/gen-cert.sh
@@ -1,2 +1,2 @@
-openssl genrsa -out key.pem 2048
-openssl req -new -x509 -key key.pem -out cert.pem -days 365
+openssl req -new -x509 -key key.pem -out cert.pem -days 365 \
+  -subj "/C=AU/ST=SomeState/L=SomeCity/O=YourOrg/OU=YourUnit/CN=localhost"
