3beb08675f88c243dd68f806b3eb21e7da8d67c2
Christopher Caston <chris@caston.id.au>
2025-10-19 19:25:19 +0800

    update

--- packages/webapp/src/routes/preferences.py
+++ packages/webapp/src/routes/preferences.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from flask_login import login_required
+from flask_login import login_required, current_user
 from datetime import datetime
 
 preferences_bp = Blueprint('preferences', __name__, url_prefix='/preferences')
@@ -7,4 +7,6 @@ preferences_bp = Blueprint('preferences', __name__, url_prefix='/preferences')
 @preferences_bp.route('/')
 @login_required
 def index():
-    return render_template('preferences/index.html', current_year=datetime.now().year)
+    return render_template('preferences/index.html',
+                         current_year=datetime.now().year,
+                         current_user=current_user)
